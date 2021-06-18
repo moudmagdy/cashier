c2e2e6183d7d710e6c13154324455f805644e493
Mahmoud Magdy <moudmagdy@hotmail.com>
2021-06-18 06:52:24 +0200

    finished settings page

--- js/pages/settings.module.js
+++ js/pages/settings.module.js
@@ -4,8 +4,12 @@ const collapseToggles = document.querySelectorAll('.collapse__toggle');
 const collapseContainers = document.querySelectorAll('.collapse__container');
 
 collapseContainers.forEach(container => {
-    window.addEventListener('load', (e) => {
-        container.style.maxHeight = container.scrollHeight + 'px';
+    window.addEventListener('load', () => {
+        if (container.parentNode.classList.contains('collapsed')) {
+            container.style.maxHeight = 0;
+        } else {
+            container.style.maxHeight = container.scrollHeight + 'px';
+        }
     });
 });
 
@@ -13,16 +17,43 @@ collapseToggles.forEach(collapseToggle => {
     collapseToggle.addEventListener('click', (e) => {
         e.preventDefault();
 
-        const collapsible = collapseToggle.closest('.collapsible');
-        const collapseContainer = collapseToggle.parentNode.nextElementSibling;
+        const parentCollapsible = collapseToggle.closest('.collapsible');
+        const siblingCollapseContainer = collapseToggle.parentNode.nextElementSibling;
 
-        collapsible.classList.toggle('collapsed');
+        let getSiblings = function (elem) {
+            // Setup siblings array and get the first sibling
+            let siblings = [];
+            let sibling = elem.parentNode.firstChild;
 
-        if (collapsible.classList.contains('collapsed')) {
-            collapseContainer.style.maxHeight = 0;
+            // Loop through each sibling and push to the array
+            while (sibling) {
+                if (sibling.nodeType === 1 && sibling !== elem) {
+                    siblings.push(sibling);
+                }
+                sibling = sibling.nextSibling
+            }
+            return siblings;
+        };
+
+        let siblings = getSiblings(parentCollapsible);
+
+        for (let sibling of siblings) {
+            if (sibling.classList.contains('collapsible')) {
+                if (!sibling.classList.contains('collapsed')) {
+                    sibling.classList.add('collapsed');
+                    sibling.querySelector('.collapse__container').style.maxHeight = 0;
+                    sibling.querySelector('.collapse__toggle').setAttribute('aria-expanded', 'false');
+                }
+            }
+        }
+
+        if (!parentCollapsible.classList.contains('collapsed')) {
+            parentCollapsible.classList.add('collapsed');
+            siblingCollapseContainer.style.maxHeight = 0;
             collapseToggle.setAttribute('aria-expanded', 'false');
         } else {
-            collapseContainer.style.maxHeight = collapseContainer.scrollHeight + 'px';
+            parentCollapsible.classList.remove('collapsed');
+            siblingCollapseContainer.style.maxHeight = siblingCollapseContainer.scrollHeight + 'px';
             collapseToggle.setAttribute('aria-expanded', 'true');
         }
     });
