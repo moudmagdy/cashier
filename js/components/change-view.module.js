8e556e16a817abbd9c77bb52d393a98c49f8dca2
Mahmoud Magdy <moudmagdy@hotmail.com>
2021-07-15 11:18:48 +0200

    updated products animation

--- js/components/change-view.module.js
+++ js/components/change-view.module.js
@@ -1,5 +1,23 @@
 const changeViewDropdownItems = document.querySelectorAll('.layout__feature + ul li a');
 const productsList = document.querySelector('.products__list');
+const products = document.querySelectorAll('.product');
+
+function animateProducts() {
+    let a = 0;
+    products.forEach((product) => {
+        setTimeout(function () {
+            product.classList.add('animate__animated', 'animate__fadeInUp');
+            product.style.opacity = '1';
+        }, a);
+        a = a + 100;
+
+        product.addEventListener('animationend', () => {
+            product.classList.remove('animate__animated', 'animate__fadeInUp');
+        });
+    });
+}
+
+window.addEventListener('load', animateProducts);
 
 changeViewDropdownItems.forEach(item => {
     item.addEventListener('click', (e) => {
@@ -7,18 +25,24 @@ changeViewDropdownItems.forEach(item => {
         item.closest('ul').querySelector('.active').classList.remove('active');
         item.classList.add('active');
         let clickedItem = item.getAttribute('data-view');
+        products.forEach((product) => {
+            product.removeAttribute('style');
+        });
         switch (clickedItem) {
             case 'grid-view':
                 productsList.classList.remove('list-view');
                 productsList.classList.remove('grid-view--no-title');
+                animateProducts();
                 break;
             case 'grid-view--no-title':
                 productsList.classList.add('grid-view--no-title');
                 productsList.classList.remove('list-view');
+                animateProducts();
                 break;
             case 'list-view':
                 productsList.classList.add('list-view');
                 productsList.classList.remove('grid-view--no-title');
+                animateProducts();
                 break;
         }
     });
